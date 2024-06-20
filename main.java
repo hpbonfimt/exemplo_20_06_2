8092933a3b7d69df1a5c4287675d9db26517843c
hugo <hugo@example.com>
2024-06-20 15:13:45 -0300

    Terceiro commit pt. 2 - Hugo

--- main.java
+++ main.java
@@ -3,6 +3,6 @@ public class main {
 	public static void main (String[] args) {
 	
 		System.out.println ("Segundo commit - hugo");
-		System.out.println ("Tericeiro commit - hugo);
+		System.out.println ("Tericeiro commit - hugo");
 	}
 }
